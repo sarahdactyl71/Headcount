b10b616dcd9090ec506829d51852bf04c42d8364
Sarah Kirk <kirk710@gmail.com>
2017-04-10 17:52:01 -0600

    runs some tests

--- lib/load_data.rb
+++ lib/load_data.rb
@@ -1,4 +1,10 @@
 require 'csv'
 
 module LoadData
+  #Load data into the program to be accessed from other places
+  #use what I learned from the event manager to do this
+  #
+  def says_hi
+    "Hello"
+  end
 end
--- test/load_data_test.rb
+++ test/load_data_test.rb
@@ -5,4 +5,10 @@ require './lib/load_data'
 
 class LoadDataTest < Minitest::Test
 
+  include LoadData
+
+  def test_what_does_it_say
+    assert_equal "Hello", says_hi
+  end
+  
 end
