04af36fadd4d4abb427b7b3723667b1182f99601
Carl Richmond <richmond.carl@outlook.com>
2017-04-11 08:57:07 -0600

    District list work

--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -1,15 +1,35 @@
 require "csv"
-require './lib/finder_module'
+require './lib/district'
 require "pry"
 
 class DistrictRepository
-include Finder
-attr_reader :data
+# include Finder
+attr_reader :data, :districts
+
+  def initialize
+    @districts = {}
+  end
 
   def load_data(args)
     @data = CSV.open args[:enrollment][:kindergarten], headers: true, header_converters: :symbol
-binding.pry
+    district_list(data)
+    binding.pry
   end
 
+
+  def find_by_name(input)
+    data.map do |row|
+      if row[:location] == input
+        district  District.new(row)
+      end
+    end
+  end
+
+  def district_list(input)
+    districts = data.map do |row|
+      row[:location]
+    end
+    binding.pry
+  end
 
 end
--- test/district_responsitory_test.rb
+++ test/district_responsitory_test.rb
@@ -16,7 +16,8 @@ class DistrictReponsitoryTest < Minitest::Test
           :kindergarten => "./data/Kindergartners in full-day program.csv"
         }
       })
-      
+    assert_equal true, dr.find_by_name("Something")
+    
     end
 
 end
