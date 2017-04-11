52b2c5258d866ebc09aa18e178c02a00af0f916a
Carl Richmond <richmond.carl@outlook.com>
2017-04-10 20:14:24 -0600

    Work on loading a file

--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -1,11 +1,17 @@
-require 'csv'
+require "csv"
 require './lib/finder_module'
 require "pry"
 
 class DistrictRepository
 include Finder
+attr_reader :data
 
-  def load_data(input)
+  def load_data(args)
+    @data = CSV.open args, headers: true, header_converters: :symbol
+    binding.pry
   end
 
 end
+
+dr = DistrictRepository.new
+dr.load_data("./data/Kindergartners in full-day program.csv")
