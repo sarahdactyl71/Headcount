0c277e07f00f5ab7b48d39d64e8be676a5d004c3
Sarah Kirk <kirk710@gmail.com>
2017-04-12 17:18:32 -0600

    adds more to headcount file

--- lib/headcount_analyst.rb
+++ lib/headcount_analyst.rb
@@ -1,4 +1,5 @@
 require "pry"
+require_relative "enrollment_repository"
 
 class HeadcountAnalyst
 
@@ -6,6 +7,32 @@ class HeadcountAnalyst
 
   def initialize(district_repository)
     @district_repository = district_repository
+    @info = {
+  :enrollment => {
+    :kindergarten => "./test/fixtures/Kindergarten_sample_data.csv"
+  }}
   end
+
+  def load_data(args)
+    data = CSV.open(args[:enrollment][:kindergarten], headers: true, header_converters: :symbol)
+  end
+
+  def data_cleaner ;end
+
+  def kindergarten_participation_rate_variation(district, comparison)
+
+  end
+
+  def year_and_rate(input)
+    info = {}
+    @data.each do |row|
+      if row[:location] == input.upcase
+        info[row[:timeframe].to_i] = row[:data].to_f
+      end
+    end
+    info
+  end
+
+
 
 end
