309490cf3e8db9c5a3d596419d3ab1b04874f303
Sarah Kirk <kirk710@gmail.com>
2017-04-12 13:02:04 -0600

    updates enrollment test file to make tests pass

--- lib/enrollment.rb
+++ lib/enrollment.rb
@@ -22,13 +22,3 @@ attr_reader :name, :kindergarten_participation
   end
 
 end
-
-# Goal 1: Build an array of these... {:name => "ACADEMY 20", :kindergarten_participation => {2010 => 0.3915, 2011 => 0.35356, 2012 => 0.2677}}
-# all_enrollment_data = []
-# CSV.foreach do |row|
-#   For each row
-#   Check if the name is already present
-#   If name is not present
-#     Add a new hash with a key of name that points to the location
-#   Always Add the year as a key under the kindergarten_participation hash with a value of enrollment rate as a percentage
-#  Goal 2: After the array of hashes is complete we need to iterate over the array and return an array of Enrollment objects
---
+++ lib/runner.rb
@@ -0,0 +1,4 @@
+require "./test/district_repository_test"
+require "./test/district_test"
+require "./test/enrollment_repository_test"
+require "./test/enrollment_test"
