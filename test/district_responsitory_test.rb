c79c4d20abf5348594f60cfa14e8e02a2ef764e5
Sarah Kirk <kirk710@gmail.com>
2017-04-12 18:30:49 -0600

    makes setup for all the test files

--- test/district_responsitory_test.rb
+++ test/district_responsitory_test.rb
@@ -4,30 +4,29 @@ require 'minitest/pride'
 require './lib/district_repository'
 
 class DistrictReponsitoryTest < Minitest::Test
-  def test_init
-    dr = DistrictRepository.new
-    assert_instance_of DistrictRepository, dr
-  end
 
-  def test_load_data
-    dr = DistrictRepository.new
+  attr_reader :dr
+
+  def setup
+    @dr = DistrictRepository.new
     dr.load_data({
         :enrollment => {
           :kindergarten => "./data/Kindergartners in full-day program.csv"
         }
       })
+  end
+
+  def test_init
+    assert_instance_of DistrictRepository, dr
+  end
+
+  def test_load_data
     district = dr.find_by_name("academy 20")
     assert_equal "ACADEMY 20", district.name
     #assert_equal true, district.enrollment_repository.kindergarten_participation_in_year(2010)
     end
 
   def test_load_data_two
-    dr = DistrictRepository.new
-    dr.load_data({
-        :enrollment => {
-          :kindergarten => "./data/Kindergartners in full-day program.csv"
-        }
-      })
     district = dr.find_by_name("JOHNSTOWN-MILLIKEN RE-5J")
     assert_equal "JOHNSTOWN-MILLIKEN RE-5J", district.name
     end
