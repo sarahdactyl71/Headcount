e7bedda66930b8400df60cd84a2b3726a06b3b16
Carl Richmond <richmond.carl@outlook.com>
2017-04-18 15:41:16 -0600

    taking average years from csv

--- lib/economic_profile.rb
+++ lib/economic_profile.rb
@@ -5,6 +5,11 @@ require 'pry'
 class EconomicProfile
 
   include Helper
+  attr_reader :median_household_income,
+              :children_in_poverty,
+              :free_or_reduced_price_lunch,
+              :title_i,
+              :name
 
   def initialize(args)
     @median_household_income = args[:median_household_income]
@@ -15,8 +20,36 @@ class EconomicProfile
   end
 
   def median_household_income_in_year(year)
+    compiler = compiler(@median_household_income, year)
+    average(compiler)
+    binding.pry
   end
 
+  def average(data)
+    numbers = []
+    data.map do |row|
+      numbers << row[:data].to_i
+    end
+    sum = numbers.reduce(0) { |a, sum| a + sum }
+    average = (sum/(numbers.count))
+    average
+  end
+
+  def compiler(key, year)
+  compiler = []
+    key.select do |row|
+      if date_range(row[:timeframe]).include?(year) && row[:location] == name
+        compiler << row
+      end
+    end
+  return compiler
+  end
+
+  def date_range(range)
+    first = range[0..3].to_i
+    last = range[5..8].to_i
+    output = (first..last).to_a
+  end
   # def is_it_valid?(input)
   #   valid_entry = [:asian, :black, :pacific_islander, :hispanic,
   #                 :native_american, :two_or_more, :white, :math,
